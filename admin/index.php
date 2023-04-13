d1767a2331730a28973a8ac3f8728546564cd101
Rogelio U <ulisescodi@gmail.com>
2023-04-12 18:10:14 -0600

    CRUD: Lectura de datos de la base de datos

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,22 @@
 
 <?php
+
+    //Importar la conexion
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir el query
+    $consulta = "SELECT * FROM propiedades";
+    
+    //Consultar la base de datos
+    $resultadoConsulta=mysqli_query($db,$consulta);
+
+    //Mostrar los resultados 
+
+
     $resultado = $_GET['mensaje'] ?? null; //traemos de la url lo que tenga mensaje, lo que hace el doble signo de interrogacion es que si no esta adopta el null
 
     require '../includes/funciones.php';
@@ -27,20 +44,25 @@
             </thead>
 
             <tbody>
+                <?php while($propiedad= mysqli_fetch_assoc($resultadoConsulta)):?>
                 <tr>
-                    <td>1</td>
-                    <td>Casa en la playa</td>
-                    <td> <img src="../imagenes/fa97021df080adbe3138c6d2106ba662.jpg" class="imagen-tabla" alt="imagen_casa"></td>
-                    <td>$1230000,00</td>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo'];?></td>
+                    <td> <img src="../imagenes/<?php echo $propiedad['imagen'];?>" class="imagen-tabla" alt="imagen_casa"></td>
+                    <td>$<?php echo $propiedad['precio']; ?></td>
                     <td>
                         <a href="#" class="boton-rojo-block">Eliminar</a>
                         <a href="#" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
+                <?php endwhile;?>
             </tbody>
         </table>
     </main>
 
     <?php 
+
+    //Cerrar la conexion
+                    mysqli_close($db);
         incluirTemplate('footer');
     ?>
