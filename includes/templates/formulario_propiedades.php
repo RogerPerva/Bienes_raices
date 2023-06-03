f6484a6eb311fb3a3b19234ced3167db90fd2be4
Rogelio U <ulisescodi@gmail.com>
2023-06-02 18:00:27 -0600

    Cambiando de Arrays a object p2

---
+++ includes/templates/formulario_propiedades.php
@@ -0,0 +1,46 @@
+<fieldset><!-- 1!-->
+                <legend>Informacion general</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text"  id="titulo" name="titulo" placeholder="Titulo propiedad" value="<?php echo s($propiedad->titulo); ?>">
+
+                <label for="precio">Precio:</label>
+                <input type="number" name="precio" placeholder="Precio" value="<?php echo s($propiedad->precio); ?>">
+                
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
+
+                <label for="descripcion">Descripcion:</label>
+                <textarea id="descripcion" name="descripcion" > <?php echo s($propiedad->descripcion); ?></textarea>
+
+            </fieldset><!-- 1!-->
+            <fieldset><!-- 2!-->
+                <legend>Informacion Propiedad</legend>
+                
+                <label for="habitaciones">Habitaciones:</label>
+                <input 
+                type="number"
+                  id="habitaciones"
+                   name="habitaciones"
+                    placeholder="Ej: 3"
+                     min="1"
+                      max="9"
+                       value="<?php echo s($habitaciones); ?>">
+                
+                <label for="wc">Baños:</label>
+                <input type="number"  id="wc" name="wc" placeholder="Ej: 4" min="1" max="9" value="<?php echo s($wc); ?>">
+                
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number"  id="estacionamiento" name="estacionamiento" placeholder="Ej: 2" min="1" max="9" value="<?php echo s($estacionamiento); ?>">
+
+            </fieldset><!-- 2!-->
+            <fieldset> <!-- 3!-->
+                <legend>Vendedor</legend>
+
+                <select name="vendedorId">
+                <option value="">--Seleccione--</option>
+                    <?php  foreach($propiedad as )= mysqli_fetch_assoc($resultado)):     ?> <!-- Nos retorna un arreglo asociativo-->
+                        <option <?php echo $vendedorId === $vendedor['id'] ?'selected':''; ?> value="<?php echo $vendedor['id']; ?>"><?php echo $vendedor['nombre']." ".$vendedor['apellido']; ?></option>
+                        <?php  endwhile;     ?>
+                </select>
+            </fieldset><!-- 3!-->
